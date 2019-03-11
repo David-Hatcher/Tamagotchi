8d7cf52ba1cc45e5d57730453d81c7b1606a4bd7
Jamie McCaw <psyral@gmail.com>
2019-03-10 20:01:05 -0400

    Creates an introduction to the player asking for gender and name

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -10,6 +10,26 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
+            GreetPlayer();
+            GetGender();
+            GetName();
+        }
+
+        static private void GreetPlayer()
+        {
+            Console.WriteLine("Say 'Hello' to your brand new friend who just hatched!");
+        }
+        static private string GetGender()
+        {
+            Console.Write("What gender is your new friend?: ");
+            string gender = Console.ReadLine();
+            return gender;
+        }
+        static private string GetName()
+        {
+            Console.Write("Lets name our new friend: ");
+            string name = Console.ReadLine();
+            return name;
         }
     }
 }
